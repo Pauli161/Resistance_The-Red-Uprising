32eac9d85379fa8389a0875c000ebb75f5a0fd45
Pauli161 <148967715+Pauli161@users.noreply.github.com>
2024-08-26 15:31:30 +0200

    Create state.py

---
+++ state.py
@@ -0,0 +1,13 @@
+class State:
+    def __init__(self):
+        self.progress = "intro"
+        self.variables = {}
+
+    def update_progress(self, progress):
+        self.progress = progress
+
+    def set_variable(self, key, value):
+        self.variables[key] = value
+
+    def get_variable(self, key):
+        return self.variables.get(key, None)
